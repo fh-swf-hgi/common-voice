6d5944ae7a497e7c0038b17edc995eff976eeaa7
hgiefers <giefers.heiner@fh-swf.de>
2024-06-20 12:28:18 +0200

    rename sentence_id param

--- server/src/lib/clip.ts
+++ server/src/lib/clip.ts
@@ -189,7 +189,8 @@ export default class Clip {
    */
   saveClip = async (request: Request, response: Response) => {
     const { client_id, headers } = request;
-    const sentenceId = headers.sentence_id as string;
+    //const sentenceId = headers.sentence_id as string;
+    const sentenceId = headers['sentence-id'] as string;
     const source = headers.source || 'unidentified';
     const format = headers['content-type'];
     const size = headers['content-length'];
